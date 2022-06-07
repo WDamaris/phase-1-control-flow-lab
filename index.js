cf5e5272bcaae1796540148445699424b7112386
WDamaris <damarisshish@gmail.com>
2022-06-07 10:08:50 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+if (ride<=400) {
+  return "This one is on me!"
+} else if (ride<=2499) {
+ return "I will gladly take your thirty bucks."
+} else if  (ride>2500) {
+  return "No can do."
+}
+}
+function ternaryCheckCity(city){ 
+return city==="NYC"?"Ok, sounds good.":"No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+switch (tip){
+  case 'generous':
+    return `Thank you so much.`;
+    break;
+   case  'not as generous':
+   return "Thank you.";
+   break;
+    case `thanks for everything`:
+      return 'Bye.';
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
